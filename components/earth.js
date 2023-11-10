0737b94bd933e51224150fada0f133f1aae401b3
antoinel74 <antoine.lansman@gmail.com>
2023-11-11 00:13:36 +0100

    add responsive

--- components/earth.js
+++ components/earth.js
@@ -1,6 +1,5 @@
 import "../style.css";
 import * as THREE from "three";
-import { DragControls } from "three/examples/jsm/controls/DragControls";
 
 window.canvas = document.getElementById("canvas");
 window.canvas.width = innerWidth;
@@ -9,9 +8,9 @@ window.iw = innerWidth;
 window.ih = innerHeight;
 
 const scene = new THREE.Scene();
-/* scene.add(new THREE.AxesHelper(5)); */
+scene.add(new THREE.AxesHelper(5));
 
-const camera = new THREE.PerspectiveCamera(70, iw / ih);
+const camera = new THREE.PerspectiveCamera(90, window.iw / window.ih);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const texture = new THREE.TextureLoader().load("assets/earth.jpg");
@@ -27,7 +26,20 @@ camera.position.set(0, 0, 2);
 light.position.set(0, 0, 2);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
-move();
+updateSize();
+window.addEventListener("resize", updateSize);
+
+function updateSize() {
+  const newWidth = window.innerWidth;
+  const newHeight = window.innerHeight;
+  window.canvas.width = newWidth;
+  window.canvas.height = newHeight;
+
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(newWidth, newHeight);
+}
 
 function move() {
   requestAnimationFrame(move);
@@ -35,3 +47,5 @@ function move() {
   mesh.rotation.x += 0.001;
   renderer.render(scene, camera);
 }
+
+move();
